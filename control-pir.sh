9b63efaf285315c00c56dfa1d686b5e98f9bcde3
Hallez <jbhallez@aol.com>
2017-06-16 14:19:12 +0200

    24/05/17 si il y a plusieurs plugins à excécuter insérer un + entre chaque demande dans le config.sh 	 possibilité de lui dire de dormir...
    Vous pourrez alors configurer vous même ceci qui est l'exemple du mien:
    PIROEILGAUCHE_GPIO="0"    # GPIO Oeil Gauche
    PIROEILDROIT_GPIO="2"     # GPIO Oeil Droit
    PIRNEZ_GPIO="3"           # GPIO Nez
    PIRDETECTEUR_GPIO="4"     # GPIO PIR
    PIR_ALARME="ON"           # Si sur ON il ne se passe rien si sur ON il
    va lire le fichir ALARME.txt (ON OU OFF) si il est à ON PIR fonctionnera
    en conséquence
    PIRLOG="ON"              # ON = enregistre tous les état de pir OFF = ne
    le fait pas
    PIRHDEBUTPARLER="07"     # A partir de quelle heure mon PIR se réveille
    ? Il faut 2 chiffres !!
    PIRHFINPARLER="21"        # A partir de quelle heure mon PIR ne parle
    plus ? Il faut 2 chiffres !!
    PIRLUMIEREOFFNUIT="22"    # A partir de quelle heure je mets en veille
    toutes mes lumières ??
    PIR_ACTION_TOUTES_LES="5"               # Toutes les (en minute) et si
    PIR = 1 j'exécute tout ce qu'il y a ci dessous:
    
    # A chaque détection du PIR à 1
    PIRACHAQUEDETECTION_ORDER="fais R2D2"
    PIRACHAQUEDETECTION_SAY=""
    
    #### Divers traitements: ####
    
    # 1)
    PIR_traitement_pour="reveil"
    PIR_DIRE_DEBUT="C'est bien on se reveil !"
    PIR_DIRE_ORDER="température chambre puis météo puis fête à qui puis
    prochainevenement puis une citation" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN="Voilà bonne journée tout le monde"
    PIR_DIRE_REP_AQH="08:00"
    
    # 2)
    PIR_traitement_pour="prochainevenement"
    PIR_DIRE_DEBUT=""
    PIR_DIRE_ORDER="prochain évènement" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN=""
    PIR_DIRE_REP_AQH="18:00"
    
    # 3)
    PIR_traitement_pour="citation"
    PIR_DIRE_DEBUT="Positif attitude:"
    PIR_DIRE_ORDER="une citation" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN=""
    PIR_DIRE_REP_AQH="12:00 15:40 19:00"
    
    # 4)
    PIR_traitement_pour="blague"
    PIR_DIRE_DEBUT="La blague du jour:"
    PIR_DIRE_ORDER="raconte une blague" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN="ça t'as plu ?"
    PIR_DIRE_REP_AQH="11:00 13:00 17:00"
    
    # 5)
    PIR_traitement_pour="pleinelune"
    PIR_DIRE_DEBUT=""
    PIR_DIRE_ORDER="prochaine pleine lune puis température chambre" # Que
    doit t-il énoncé  ?
    PIR_DIRE_FIN=""
    PIR_DIRE_REP_AQH="20:35"
    
    # 6)
    PIR_traitement_pour="boldecafe"
    PIR_DIRE_DEBUT="prépare un bol de café Préparation d'un bol de café en
    cours pour se réveiller ?"
    PIR_DIRE_ORDER="" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN="Bon appétit"
    PIR_DIRE_REP_AQH="08:00"
    
    # 7)
    PIR_traitement_pour="tassecafe"
    PIR_DIRE_DEBUT="C'est l'heure de la pause café...prépare une tasse de
    café"
    PIR_DIRE_ORDER="" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN="Bon appétit"
    PIR_DIRE_REP_AQH="13:00"
    
    # 8)
    PIR_traitement_pour="temperature"
    PIR_DIRE_DEBUT="Température"
    PIR_DIRE_ORDER="température chambre" # Que doit t-il énoncé  ?
    PIR_DIRE_FIN=""
    PIR_DIRE_REP_AQH="20:00"
    
    Le soir à 20h00 il se met en mode silencieux
    Le soir à 22 heures il coupe ses lumières et dort
    Le matin à 7h00 il allume son "nez" pour dire qu'il est opérationnel et
    reprend la parole
    
    A partir de 8h00 si je passe devant le détecteur de mouvement il me dit:
    La météo (plugin), le prochain évènement (plugin) et un proverbe
    (plugin)
    
    A 17h00 il raconte une blague
    
    Dans la journée si je suis trop devant il me demande de dégourdir les
    jambes, il me parle justep our dire bonjour ou comment ca va...
    
    A midi, 12h40 et 20h il me sort une citation,

--- control-pir.sh
+++ control-pir.sh
@@ -1,14 +1,63 @@
 #!/bin/bash
 
-RELANCEPIR=`ps -aux | grep control-pir_ok.sh | wc -l`
-# if [[ "$RELANCEPIR" -ge 2 ]]; then 
-if [[ "$RELANCEPIR" -ge 1 ]]; then 
-echo "Fonctionne déja en fond de tâche..."
-killall control-pir_ok.sh; 
-sleep 5
+varpirconfigdodo="/home/pi/jarvis/plugins_installed/jarvis-plug-PIR/pause_onOUoff.txt"
+varpirretourconsole="/home/pi/jarvis/plugins_installed/jarvis-plug-PIR/PIRRETOURCONSOLE.txt"
+
+if test -d "$varpirretourconsole"; then 
+PIRRETOURCONSOLE=`cat $varpirretourconsole`
+else
+echo "ON" > $varpirretourconsole
+PIRRETOURCONSOLE="ON"
 fi
-/home/pi/jarvis/plugins_installed/jarvis-plug-PIR/control-pir_ok.sh &
 
+if test -d "$varpirconfigdodo"; then 
+MesurePIR_reveil=`cat $varpirconfigdodo`
+else
+MesurePIR_reveil="ON"
+echo "ON" > $varpirconfigdodo
+fi
 
+echo "$MesurePIR_reveil"
+if [[ "$MesurePIR_reveil" == "ON" ]]; then
+# if test -z "$MesurePIR_reveil"; then 
 
+etat_courant="0"
+etat_precedant="0"
+if [[ "$PIRRETOURCONSOLE" == "ON" ]]; then
+echo " Prêt "
+fi
 
+while true
+do
+
+etat_courant=`gpio read 4`
+
+if [ "$etat_courant" == "1" ] && [ "$etat_precedant" == "0" ]
+then
+if [[ "$PIRRETOURCONSOLE" == "ON" ]]; then
+echo " Mouvement détécté "
+fi
+date=`date +"%Y_%m_%d-%H:%M:%S"`
+if [[ "$PIRRETOURCONSOLE" == "ON" ]]; then
+echo "$date"
+fi
+
+# /home/pi/jarvis/jarvis.sh -x "mesurepir"
+jarvis -x "mesurepir"
+
+# raspistill -tl 100 -o /home/pi/image_motion/$date%04d.jpg -t 5000 -w 800 -h 600
+etat_precedant="1"
+
+elif [ "$etat_courant" == "0" ] && [ "$etat_precedant" == "1" ]
+then
+if [[ "$PIRRETOURCONSOLE" == "ON" ]]; then
+echo " Prêt "
+fi
+etat_precedant="0"
+# sleep +180s
+# sleep +10s
+fi
+
+sleep +0.1s
+done
+fi
